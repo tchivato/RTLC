55f9a71754136911f517b5fbe814b916c24a7a32
tchivato <48048302+tchivato@users.noreply.github.com>
2025-06-05 12:24:03 +0200

    Update RTLCcontrol.py
    Adds deviceID and firmware and RTLC control versions. Adds solvent  information. Solves bug where date wouldn't be registered on the csv

--- RTLCcontrol.py
+++ RTLCcontrol.py
@@ -8,7 +8,9 @@ import time
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import hashlib
+import re
 
+version = "v1.2" #current RTLCcontrol version
 current_datetime = time.localtime()
 default_batch = f"{current_datetime.tm_mday:02d}{current_datetime.tm_mon:02d}{current_datetime.tm_year % 100:02d}RF"
 
@@ -22,7 +24,9 @@ def send_data():
     batch = entry_batch.get()
     operator = entry_operator.get()
     distance = entry_distance.get()
-    time = entry_time.get()
+    origin = entry_origin.get()
+    front = entry_front.get()
+    acq_time = entry_acq_time.get()
     port = combobox_ports.get()
 
     # Field validation
@@ -35,7 +39,13 @@ def send_data():
     if not distance.isdigit() or not (10 <= int(distance) <= 120):
         messagebox.showerror("Error", "Distance must be between 10 and 120 mm.")
         return
-    if not time.isdigit() or int(time) < 1:
+    if not origin.isdigit() or not (1 <= int(origin) <= 120):
+        messagebox.showerror("Error", "Solvent origin must be between 1 and 120 mm.")
+        return
+    if not front.isdigit() or not (1 <= int(front) <= 120):
+        messagebox.showerror("Error", "Solvent front must be between 1 and 120 mm.")
+        return    
+    if not acq_time.isdigit() or int(acq_time) < 1:
         messagebox.showerror("Error", "Time must be at least 1 minute.")
         return
     if not port:
@@ -45,7 +55,7 @@ def send_data():
     try:
         # Serial port settings
         ser = serial.Serial(port, baudrate=9600, timeout=1)
-        data = f"{batch},{operator},{distance},{time}\n"
+        data = f"{batch},{operator},{distance},{acq_time}\n"
         ser.write(data.encode())  
         ser.flush()
 
@@ -64,7 +74,7 @@ def send_data():
         root.update()
 
         def end_acquisition():
-            """Ends acquisition and saves results."""
+            #Ends acquisition and saves results.       
             if ser and ser.is_open:
                 ser.close()
 
@@ -77,7 +87,7 @@ def send_data():
             
             # Adds variables as metadata
             with open(csv_name, mode='a', encoding='utf-8') as archive:
-                archive.write(f"Batch:;{batch}\nOperator:;{operator}\nRange:;{distance} mm\nAcquisition time:;{time} min\n")
+                archive.write(f"Batch:;{batch}\nOperator:;{operator}\nRange:;{distance} mm\nSolvent origin:;{origin} mm\nSolvent front:;{front} mm\nAcquisition time:;{acq_time} min\nDevice:;{deviceID}\nFirmware:;{firmware}\nRTLC Control version:;{version}\n")
                 archive.write(f"Date:;{time.strftime("%d/%m/%Y, %H:%M:%S", time.localtime())}\n")
 
             # Adds hash to CSV
@@ -93,11 +103,16 @@ def send_data():
             btn_run.config(text="START", state="normal")
 
         def read_port():
-            """Reads serial port and updates graph."""
+            #Reads serial port and updates graph.
+            global firmware, deviceID
             if ser.in_waiting:
                 try:
                     line = ser.readline().decode('utf-8', errors='ignore').strip()
-                    if line == "end":
+                    if line.startswith("end_"):
+                        try:
+                            firmware, deviceID = line[4:].split("_",1)
+                        except:
+                            pass
                         end_acquisition()
                         return
                     if line:
@@ -128,7 +143,7 @@ def send_data():
         btn_run.config(text="START", state="normal")
 
 root = tk.Tk()
-root.title("RTLC Control")
+root.title(f"RTLC Control {version}")
 
 # Labels and data entry
 tk.Label(root, text="Batch:", anchor="e").grid(row=0, column=0, padx=10, pady=5, sticky="e")
@@ -142,25 +157,36 @@ entry_operator.grid(row=1, column=1, padx=10, pady=5)
 
 tk.Label(root, text="Range (mm):", anchor="e").grid(row=2, column=0, padx=10, pady=5, sticky="e")
 entry_distance = tk.Entry(root)
+entry_distance.insert(0, 120)
 entry_distance.grid(row=2, column=1, padx=10, pady=5)
 
-tk.Label(root, text="Time (min):", anchor="e").grid(row=3, column=0, padx=10, pady=5, sticky="e")
-entry_time = tk.Entry(root)
-entry_time.grid(row=3, column=1, padx=10, pady=5)
+tk.Label(root, text="Solvent origin (mm):", anchor="e").grid(row=3, column=0, padx=10, pady=5, sticky="e")
+entry_origin = tk.Entry(root)
+entry_origin.insert(0, 10)
+entry_origin.grid(row=3, column=1, padx=10, pady=5)
 
-tk.Label(root, text="COM port:", anchor="e").grid(row=4, column=0, padx=10, pady=5, sticky="e")
+tk.Label(root, text="Solvent front (mm):", anchor="e").grid(row=4, column=0, padx=10, pady=5, sticky="e")
+entry_front = tk.Entry(root)
+entry_front.insert(0, 90)
+entry_front.grid(row=4, column=1, padx=10, pady=5)
+
+tk.Label(root, text="Time (min):", anchor="e").grid(row=5, column=0, padx=10, pady=5, sticky="e")
+entry_acq_time = tk.Entry(root)
+entry_acq_time.grid(row=5, column=1, padx=10, pady=5)
+
+tk.Label(root, text="COM port:", anchor="e").grid(row=6, column=0, padx=10, pady=5, sticky="e")
 ports = list_ports()
 combobox_ports = ttk.Combobox(root, values=ports, state="readonly")
-combobox_ports.grid(row=4, column=1, padx=10, pady=5)
+combobox_ports.grid(row=6, column=1, padx=10, pady=5)
 
 btn_run = tk.Button(root, text="START", command=send_data, font=("Helvetica", 15, "bold"))
-btn_run.grid(row=6, column=0, columnspan=2, pady=20, sticky="ew")
+btn_run.grid(row=8, column=0, columnspan=2, pady=20, sticky="ew")
 
 # Graph area
 fig = Figure(figsize=(5, 3), dpi=100)
 ax = fig.add_subplot(111)
 canvas = FigureCanvasTkAgg(fig, master=root)
-canvas.get_tk_widget().grid(row=8, column=0, columnspan=2, pady=10)
+canvas.get_tk_widget().grid(row=10, column=0, columnspan=2, pady=10)
 
 
 root.mainloop()
